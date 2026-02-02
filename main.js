46548748279b2f2fea983e130ec3f2e76ab733a9
paolo-della-fera <paolodf2005@gmail.com>
2026-02-02 16:46:11 +0100

    add price calculation with discounts

--- main.js
+++ main.js
@@ -18,9 +18,30 @@ console.log("Età:", age);
 // prezzo definito in base hai chilometri €0.21 
 const priceKm = 0.21;
 
-//stampo il prezzo per chilometro 
+// stampo il prezzo per chilometro 
 console.log("Prezzo per km:", priceKm);
 
 
 // ora mi calcolo il prezzo base senza sconti 
 let price = km * priceKm
+
+
+// effettuo un controllo dell'età del passeggerio per capire se va applicato uno sconto
+
+// SE il passeggero è minorenna ha lo sconto del %20
+if (age < 18) {
+    price = price * 0.8;
+}
+
+// SE il passeggero è over 65 ha lo sconto del %40
+else if (age >= 65) {
+    price = price * 0.6;
+}
+
+// ALTRIMETI paga il prezzo pieno del biglietto 
+else {
+    console.log("Nessuno sconto applicato, prezzo intero");
+}
+
+// stampo il prezzo totale 
+console.log("Totale da pagare:", price.toFixed(2) + "€");

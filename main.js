674a846a92f05e3a32cc86c2528bb073c5e8f9e4
paolo-della-fera <paolodf2005@gmail.com>
2026-02-02 15:53:55 +0100

    add the price for kilometer

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
 // chiedo all'utente il numero di chilometri da percorrere
-// i numeri vengono letti con il punto quindi sostituisco la virgola con il punto per semplificare l'inserimento dei chilometri da parte dell'utente 
+
+/*
+
+i numeri vengono letti con il punto quindi sostituisco la virgola con il punto per semplificare l'inserimento dei chilometri da parte dell'utente
+
+*/
 const km = parseFloat(prompt("Inserisci i chilometri da percorrere").replace(",", "."));
 
 
@@ -9,3 +14,11 @@ const age = parseInt(prompt("Inserisci l'età del passeggero"));
 
 // stampo l'eta e i chilometri da perccorrere
 console.log(km, age);
+
+
+// prezzo definito in base hai chilometri €0.21 
+const priceKm = 0.21;
+
+
+
+
